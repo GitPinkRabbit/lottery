a6f4b94534a21a0929f4aea69ccaa20987fa1bb0
PinkRabbit <pinkrabbit030325@gmail.com>
2024-08-30 02:35:39 +0800

    Ver 2

--- lottery.py
+++ lottery.py
@@ -1,4 +1,4 @@
-# lottery.py. Version 20240711
+# lottery.py. Version 20240829
 
 import sys
 import csv
@@ -14,9 +14,9 @@ csv_file_encoding = 'utf-8-sig'
 # Settings
 contest_types = ['J', 'S', 'X']
 prize_count_by_type = {
-	'J': [0, 0, 0, 5],
-	'S': [0, 1, 4, 5],
-	'X': [1, 4, 9, 5]
+	'J': [0, 0, 0],
+	'S': [0, 1, 4],
+	'X': [1, 4, 9]
 }
 prize_names = ['特等奖', '一等奖', '二等奖']
 additional_prize_name = '幸运奖'
@@ -41,6 +41,7 @@ with open(filename, encoding=csv_file_encoding, newline='') as csv_file:
 	random.seed(rand_seed + time_seed)
 	participants.pop(0)
 
+	number_of_valid_participants = 0
 	prize_lists = [[] for _ in range(len(prize_names) + 1)]
 	choices = []
 	weights = []
@@ -48,6 +49,8 @@ with open(filename, encoding=csv_file_encoding, newline='') as csv_file:
 		rank = int(participant[0])
 		name = participant[1]
 		score = int(participant[6])
+		if score > 0:
+			number_of_valid_participants += 1
 		chosen = False
 		for i in range(len(prize_names)):
 			if rank <= prize_count_by_type[contest_type][i]:
@@ -57,7 +60,8 @@ with open(filename, encoding=csv_file_encoding, newline='') as csv_file:
 		if not chosen:
 			choices.append((name, score, rank))
 			weights.append(score ** 2)
-	for _ in range(prize_count_by_type[contest_type][-1]):
+	number_of_additional_prize = number_of_valid_participants // 50
+	for _ in range(number_of_additional_prize):
 		chosen_participant = random.choices(choices, weights)[0]
 		prize_lists[-1].append(chosen_participant)
 		index = choices.index(chosen_participant)
